78bf0b21d7e9d871cdbb23b7ef7f9fd4714cc60d
sravanthimodepu <sravanthimodepu@gmail.com>
2021-03-19 10:20:07 +0530

    added content to the experiment

---
+++ experiment/manual.md
@@ -0,0 +1,16 @@
+There are certain parameters to be initialized and the procedure detailed here can be followed.
+
+STEP 1:Select the molecule .
+<img src="images/poly1.png">
+
+STEP 2: Click on 'start' tab to initialize the experiment.
+<img src"images/poly5.png">
+
+STEP 3: The coordinates of particles can be changed by changing the position of slider.
+<img src="images/poly2.png">
+
+STEP 4: The force constant values can be varied by chaniging the position of sliders.
+<img src="images/poly3.png">
+
+STEP 5: The plot of change in energy with respect to change in position of magnet coordinates is seen.
+<img src="images/Screenshot-3.png">
--- experiment/pretest.js
+++ experiment/pretest.js
@@ -89,23 +89,60 @@
 
 
      const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
+            question: "1. The magnitude of energy required to change the conformation of a molecule is called dihedral (or torsional) energy.", ///// Write the question inside double quotes
             answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                a: "True", ///// Write the option 1 inside double quotes
+                b: "False", ///// Write the option 2 inside double quotes
             },
             correctAnswer: "a" ///// Write the correct option inside double quotes
         },
 
-    {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
-      answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
-      correctAnswer: "c"                ///// Write the correct option inside double quotes
-    },
+{
+            question: "2. Gauche conformation forces the large methyl groups in Butane to be near each other, resulting in steric strain. ", ///// Write the question inside double quotes
+            answers: {
+                a: "True", ///// Write the option 1 inside double quotes
+                b: "False", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "a" ///// Write the correct option inside double quotes
+        },
 
+{
+            question: "3. The potential energy at large separation's is essentially the bond energy ", ///// Write the question inside double quotes
+            answers: {
+                a: "True", ///// Write the option 1 inside double quotes
+                b: "False", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "a" ///// Write the correct option inside double quotes
+        },
+
+
+{
+            question: "4. The dihedral angle of four atoms ¦Õ varies only the distance between the first and third atoms; the other interatomic distances are constrained by the chemical bond lengths and bond angles ", ///// Write the question inside double quotes
+            answers: {
+                a: "True", ///// Write the option 1 inside double quotes
+                b: "False", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "a" ///// Write the correct option inside double quotes
+        },
+
+{
+            question: "5. Steric strain is the repulsive interaction that occurs when two groups are forced to be closer to each other than their atomic radii allow;", ///// Write the question inside double quotes
+            answers: {
+                a: "True", ///// Write the option 1 inside double quotes
+                b: "False", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "a" ///// Write the correct option inside double quotes
+        },			  
+
+{
+            question: "1. The magnitude of energy required to change the conformation of a molecule is called dihedral (or torsional) energy.", ///// Write the question inside double quotes
+            answers: {
+                a: "True", ///// Write the option 1 inside double quotes
+                b: "False", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "a" ///// Write the correct option inside double quotes
+        }
+			  
 
 
      ];
